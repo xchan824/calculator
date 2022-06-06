09858f42709f13b0d3741d469e4b2ededddea3ae
Christian Paulo Gomonit <xchan824@gmail.com>
2022-06-06 10:20:32 +0800

    final

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-let previousInput = document.getElementById('previous-input');
 let currentInput = document.getElementById('current-input');
 
 let buttons = Array.from(document.getElementsByClassName('button'));
@@ -8,8 +7,45 @@ console.log(buttons);
 buttons.map(button => {
     button.addEventListener('click', (e) => {
         switch(e.target.innerText) {
+            case 'C':
+                currentInput.innerText = '0';
+                break;
+            case '←':
+                currentInput.innerText = currentInput.innerText.slice(0, -1);
+                if (currentInput.innerText == '') {
+                    currentInput.innerText = '0';
+                }
+                break;
+            case '=':
+                currentInput.innerText = Function("return " + currentInput.innerText)();
+                console.log(currentInput.innerText);
+                break;
+            case '.':
+                if (!currentInput.innerText.includes('.')) {
+                    currentInput.innerText += e.target.innerText;
+                } else {
+                    break;
+                }
+                break;
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+                if (!currentInput.innerText.includes('+')
+                    && !currentInput.innerText.includes('-')
+                    && !currentInput.innerText.includes('*')
+                    && !currentInput.innerText.includes('/')) {
+                    currentInput.innerText += e.target.innerText;
+                } else {
+                    break;
+                }
+                break;
             default:
-                currentInput.innerText += e.target.innerText;
+                if (currentInput.innerText == '0') {
+                    currentInput.innerText = e.target.innerText;
+                } else {
+                    currentInput.innerText += e.target.innerText;
+                }
         }
     })
 });
